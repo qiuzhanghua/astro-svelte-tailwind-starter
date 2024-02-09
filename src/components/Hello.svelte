30fa4743487c38fc3bd86405f93b1cb494d9bfb7
qiuzhanghua <qiuzhanghua@icloud.com>
2024-02-09 10:11:37 +0800

    feat: client:only for svelte components

--- src/components/Hello.svelte
+++ src/components/Hello.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
-	export let name: string = 'world';
+  export let name: string = 'world';
+  export let fontSize: number = 64;
+  setInterval(() => {
+    if (fontSize === 64) {
+      fontSize = 16;
+    } else {
+      fontSize += 1;
+    }
+  }, 30);
 </script>
 
-<div class="bg-blue-700 text-white">Hello {name}, from Svelte</div>
+<div style="font-size: {fontSize}px" class="bg-blue-700 text-white">
+  Hello {name}, from Svelte
+</div>

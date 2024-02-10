0acf1ac833cf36ecd125dff9edfdc854168e3c49
qiuzhanghua <qiuzhanghua@icloud.com>
2024-02-10 15:23:53 +0800

    feat: custom event

---
+++ src/components/Custom.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Form from './Form.svelte';
+
+  function onFormSubmit(e: CustomEvent) {
+    const { name, age } = e.detail;
+    console.log(name, age);
+  }
+</script>
+
+<Form on:form_submit={onFormSubmit} />
---
+++ src/components/Form.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let name = 'blue';
+  let age = 30;
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+
+  function handleSubmit(e: Event) {
+    dispatch('form_submit', { name, age });
+  }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <label for="name">Name:</label>
+  <input type="text" id="name" bind:value={name} />
+
+  <label for="age">Age:</label>
+  <input type="number" id="age" bind:value={age} />
+
+  <button type="submit">Submit</button>
+</form>
